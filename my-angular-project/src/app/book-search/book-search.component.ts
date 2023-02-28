1754d438675cef59b8749ba756984dcf6ff3b903
Hanna Lundström <hannalundstrom@Hannas-MacBook-Air.local>
2023-02-28 17:45:52 +0100

    Lagt till lite grejor

--- my-angular-project/src/app/book-search/book-search.component.ts
+++ my-angular-project/src/app/book-search/book-search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-book-search',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-search.component.css']
 })
 export class BookSearchComponent {
-
-}
+ 
+    genre = this._formBuilder.group({
+      Fantasy: false,
+      Bibliografi: false,
+      Deckare: false,
+    });
+  
+  
+    firstForm = new FormControl('');
+    time = new FormControl('');
+    rating = new FormControl('');
+    length= new FormControl('');
+  
+  
+    constructor(private _formBuilder: FormBuilder) {}
+  
+  }
+  
