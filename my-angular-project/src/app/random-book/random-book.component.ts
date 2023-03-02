acee9579559d97994691125d3e22ac86ea888568
Adam Stenstrom <ad5747st-s@elg-10.e.lth.lu.se>
2023-03-02 17:11:03 +0100

    Change some functionality in random book, needs more testing

--- my-angular-project/src/app/random-book/random-book.component.ts
+++ my-angular-project/src/app/random-book/random-book.component.ts
@@ -10,10 +10,15 @@ import { Observable } from 'rxjs';
 })
 export class RandomBookComponent {
     constructor(private _http:HttpClient) {}
-    newData:any;
+    newData:any = {} as any;
+
+
+    // curl -X GET 'https://openlibrary.org/search/subjects.json?q=fantasy&language=eng' => Get all with fantasy
+    // curl -X GET 'https://openlibrary.org/search.json?q=first_publish_year%253A%5B1200+TO+1400%5D' => Get all within 1200 to 1400 publisher year
+
 
     getData(){
-        return this._http.get('https://openlibrary.org/books/OL28549292M.json');
+        return this._http.get('https://openlibrary.org/search.json?q=*&subject=fantasy&sort=random&');
     }
 
     ngOnInit(){
